10f10d132fa4baa3e94ea7ab6d40161b245c35a0
michel van dijck <michvd92@gmail.com>
2016-11-08 13:40:49 +0100

    user controller WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,39 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
   end
 
   def edit
   end
 
   def update
+    @user.update(user_params)
   end
 
   def destroy
+    @user.destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :email, :encrypted_password)
   end
 end

d3b5cc920578e7630e363051b55fdda2bab82801
Dana Mol <dana.mol6@gmail.com>
2016-11-08 14:00:17 +0100

    Generate devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews, dependent: :destroy
   has_many :boards, dependent: :destroy
   has_many :bookings, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :encrypted_password, presence: true
   validates :address, presence: true
 end

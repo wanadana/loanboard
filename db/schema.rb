db8e194f88937cea262038f47c2f7d5f0accce2a
Dana Mol <dana.mol6@gmail.com>
2016-11-08 13:09:52 +0100

    Remove name column & added first & last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108103718) do
+ActiveRecord::Schema.define(version: 20161108112103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,13 +55,14 @@ ActiveRecord::Schema.define(version: 20161108103718) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email"
     t.string   "encrypted_password"
     t.string   "role"
     t.string   "address"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_foreign_key "availabilities", "boards"

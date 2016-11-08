a73baf4a44f66fe3ea56e3c46681bf68c4745d79
michel van dijck <michvd92@gmail.com>
2016-11-08 11:55:23 +0100

    review - min length

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :board
   belongs_to :user
-  validates :content, presence: true
+  validates :content, length: { minimum: 5 }, presence: true
   validates :board_id, presence: true
   validates :user_id, presence: true
 end

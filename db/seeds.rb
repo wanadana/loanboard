269b5c151aa1e64e99f86c3fdd6f6dbac06880c0
Kyle Jobbins <kyle@jobbins.net>
2016-11-08 17:55:46 +0100

    generate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(first_name: 'Tony', last_name: 'Stark', role: 'renter', address: '420 down under', email: 'tony@stark.com')
+u2 = User.create(first_name: 'Carla', last_name: 'Carla', role: 'renter', address: '420 Amsterdam', email: 'carla@carla.com')
+u3 = User.create(first_name: 'Chicken', last_name: 'Man', role: 'owner', address: '420 Under down', email: 'chicken@man.com')
+
+b1 = Board.create(description: 'long', price: '420', category: 'Longboard', user: u3)
+b2 = Board.create(description: 'penny', price: '42.0', category: 'Pennyboard', user: u3)
+
+r1 = Review.create(content: 'this board was long.', user: u1, board: b1)
